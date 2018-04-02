37313ea9664e9a8c9144643c167ef46000c40e5e
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-02 17:42:57 -0300

    test: Added a test to create a user.

--- users/test/test-helper.js
+++ users/test/test-helper.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-    .once('open', () => console.log('Good to go!'))
-    .on('error', error => console.warn('Error', error));
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', error => console.warn('Error', error));
+});
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        done();
+    });
+});
