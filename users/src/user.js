b0ad90ba9159020549d148f564e343b00133abc5
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-03 08:55:19 -0300

    feat: Added a postCount property for the user.

--- users/src/user.js
+++ users/src/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: String
+    name: String,
+    postCount: Number
 });
 
 const User = mongoose.model('user', UserSchema);
