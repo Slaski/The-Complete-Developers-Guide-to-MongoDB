638530b64e80befdb58cb76d9788fbeec38d0669
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-03 11:59:36 -0300

    feat: Added validation to the user model.

--- users/src/user.js
+++ users/src/user.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: [true, 'Name is required.'],
+        validate: {
+            validator: (name) => name.length > 2,
+            message: 'Name must be longer than 2 characters.'
+        }
+    },
     postCount: Number
 });
 
