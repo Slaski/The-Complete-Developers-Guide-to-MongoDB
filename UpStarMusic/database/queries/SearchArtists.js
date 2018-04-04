74eb6985e3d2e801471e770804990d0264e8767f
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-04 18:33:28 -0300

    feat: Implements the name filter in the SearchArtists function.

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -27,12 +27,17 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
 const buildQuery = criteria => {
   const query = {};
 
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
   if (criteria.age) {
     query.age = {
       $gte: criteria.age.min,
       $lte: criteria.age.max
     };
   }
+
   if (criteria.yearsActive) {
     query.yearsActive = {
       $gte: criteria.yearsActive.min,
