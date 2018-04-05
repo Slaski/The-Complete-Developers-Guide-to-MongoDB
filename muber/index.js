fe906576ba0ad56f740a48ede69eb7c4340f81d0
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-05 10:33:43 -0300

    chore: Add a simple api route.

--- muber/index.js
+++ muber/index.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
+app.get('/api', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
 app.listen(3050, () => {
-    console.log('Running on port 3050.');
+  console.log('Running on port 3050.');
 });
